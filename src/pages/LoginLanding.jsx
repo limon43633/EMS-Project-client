463457b5f2ae2913c3c13d77cf86eeee12861981
Hassibur Rahman <md9178558@gmail.com>
2026-04-07 13:43:36 +0600

    added route

---
+++ src/pages/LoginLanding.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginLanding = () => {
+    return (
+        <div>
+           Login Landing Page 
+        </div>
+    );
+};
+
+export default LoginLanding;
---
+++ src/pages/PrintPaySlip.jsx
@@ -0,0 +1,11 @@
+
+
+const PrintPaySlip = () => {
+    return (
+        <div>
+            Print Pay Slip
+        </div>
+    );
+};
+
+export default PrintPaySlip;
