e4f9ed2cb0189cf2586876d19eeb19d678c10ba8
Hassibur Rahman <md9178558@gmail.com>
2026-04-07 23:24:04 +0600

    login form add

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,9 +3,14 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <div>
-            Layout
-            <Outlet />
+        <div className='flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30'>
+            <p>sidebar</p>
+            <main className='flex-1 overflow-y-auto'>
+                <div className='p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto bg-amber-400'>
+                    <Outlet />
+                </div>
+            </main>
+            
         </div>
     );
 };
